1ff297095ae75ae72c4dca38fbbc96c7f06a21af
Sergey Yavorsky <yavorskyserge@gmail.com>
2023-12-31 10:58:13 +0300

    add grow_spee

---
+++ growth_rate.py
@@ -0,0 +1,22 @@
+from math import log, sqrt, factorial
+
+n = 1000
+
+print(1, log(log(n, 2)))
+print(2, sqrt(log(n, 4)))
+print(3, log(n, 3))
+print(4, (log(n, 2)) ** 2)
+print(5, sqrt(n))
+print(6, n / log(n, 5))
+print(7, log(factorial(n), 2))
+print(8, 3 ** log(n, 2))
+print(9, n ** 2)
+print(10, 7 ** log(n, 2))
+print(11, log(n, 2) ** log(n, 2))
+print(12, n ** log(n, 2))
+print(13, n ** sqrt(n))
+print(14, 2 ** n)
+print(15, 4 ** n)
+print(16, 2 ** (3 * n))
+print(17, factorial(n))
+# print(18, 2 ** 2 ** n)
